b5f9621ce7a5dd5fdef1470e14d7cd629ced0d77
knulii <55326439+knulii@users.noreply.github.com>
2024-10-09 10:18:37 -0700

    Update system.sh

--- system.sh
+++ system.sh
@@ -62,7 +62,7 @@ echo -e "$GREEN Done...$COL_RESET"
 echo -e " Installing MariaDB Repository...$COL_RESET"
 hide_output sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 if [[ ("$DISTRO" == "16") ]]; then
-  sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://mirror.one.com/mariadb/repo/10.4/ubuntu xenial main' >/dev/null 2>&1
+  sudo add-apt-update >/dev/null 2>&1
 else
   sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.one.com/mariadb/repo/10.4/ubuntu bionic main' >/dev/null 2>&1
 fi
